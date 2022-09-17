7a21439be3384f17a5b8c21c2fe4ad99726f9aff
Dmitry Simdyanov <newtemnota108@gmail.com>
2022-09-17 19:46:48 +0400

    old recipe

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int eaters = 5; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
-        int potatoes = 4; // картофелин
-        int chicken = 8; // куриных бёдер
-        int spices = 9; // ложек специй
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
